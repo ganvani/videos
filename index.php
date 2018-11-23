b7721126eff3348310e5ac29b013c3cf4b87df09
Ishan Vyas <vyasishanatc194@gmail.com>
2018-11-23 19:23:27 +0530

    video create different language with audio and subtitle

--- index.php
+++ index.php
@@ -5,62 +5,59 @@
     * I have create this with static data like srt and mp4 video files.
     * This script will create one video (mp4 format) file with multiple subtitles and diff. languages
     */
-
+   
    /* i.e : test.mp4 i have already one video file without any sub title */
-   $videoFile = "D:\\videoplayback.mp4"; 
-   /* i.e : en.srt it is srt file for english langauage */
-   $en_srtFile = "D:\\en.srt";
-   /* i.e : gu.srt it is srt file for gujarati langauage */
-   $gu_srtFile = "D:\\gu.srt";
+   $videoFile = "D:\\xampp\\htdocs\\video-qbix\\videos\\sample\\sample.mp4"; 
 
-   // This array used for add ffmpeg parameters.
-   $en_commands = array(
+   $hindi_audioFile = "D:\\xampp\\htdocs\\video-qbix\\videos\\sample\\hindi.mp3";
+   $hi_srtFile = "D:\\xampp\\htdocs\\video-qbix\\videos\\sample\\hi.srt";
+   $outfile_hindi="D:\\xampp\\htdocs\\video-qbix\\videos\\sample\\hi.mp4";
+
+   $english_audioFile = "D:\\xampp\\htdocs\\video-qbix\\videos\\sample\\english.mp3";
+   $en_srtFile = "D:\\xampp\\htdocs\\video-qbix\\videos\\sample\\en.srt";
+   $outfile_english="D:\\xampp\\htdocs\\video-qbix\\videos\\sample\\en.mp4";
+
+   $hi_commands = array(
+    '-i '.$videoFile,
+    '-i '.$hindi_audioFile,
+    '-i '.$hi_srtFile,
+    '-map 0',
+    '-map 1',
+    '-c copy',
+    '-c:s mov_text',
+    '-c:v copy',
+    '-c:a aac',
+    '-strict',
+    'experimental',
+    $outfile_hindi,
+    );
+
+    $hi_str='C:\\FFmpeg\\bin\\ffmpeg.exe '.implode(' ',$hi_commands); 
+    echo shell_exec($hi_str);
+    echo "hindi.mp4 file created.<br/>";
+  
+    $en_commands = array(
         '-i '.$videoFile,
+        '-i '.$english_audioFile,
         '-i '.$en_srtFile,
-        '-map 0',
+         '-map 0',
         '-map 1',
         '-c copy',
         '-c:s mov_text',
-        'D:\\en.mp4'
-    );
+        '-c:v copy',
+        '-c:a aac',
+        '-strict',
+        'experimental',
+        $outfile_english,
+        );
 
+        
     $en_str='C:\\FFmpeg\\bin\\ffmpeg.exe '.implode(' ',$en_commands);
-
-    /**
-     * This command execute as input three file 
-     *      1) video file 
-     *      2) multiple srt file
-     *  Output :
-     *      Generate new video file with multiple subtitles with different language.
-     */
-    
+   
     echo shell_exec($en_str);
-    echo "en.mp4 file created.<br/>";
-    exit;
+    echo "english.mp4 file created.<br/>";
 
-    $gu_commands = array(
-        '-i '.$videoFile,
-        '-i '.$gu_srtFile,
-        '-map 0',
-        '-map 1',
-        '-c copy',
-        '-c:s mov_text',
-        'D:\\gu.mp4'
-    );
-
-    $gu_str='C:\\FFmpeg\\bin\\ffmpeg.exe '.implode(' ',$gu_commands);
-
-    /**
-     * This command execute as input three file 
-     *      1) video file 
-     *      2) multiple srt file
-     *  Output :
-     *      Generate new video file with multiple subtitles with different language.
-     */
-    
-    echo shell_exec($gu_str);
-    echo "gu.mp4 file created.<br/>";
-    exit;
+    exit;   
 
 ?>
 
